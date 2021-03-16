a0d9671f542ee8b6dbdee4a95abcdefe2f8d327b
claire ogesen <claire.ogesen.co@gmail.com>
2021-03-15 18:08:32 -0700

    added map javascript not working

---
+++ assets/stalls.js
@@ -0,0 +1,16 @@
+// var map: google.maps.Map;
+
+// function initMap(): void {
+//   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+//     center: { lat: 33.428, lng: -111.936 },
+//     zoom: 8,
+//   });
+// }
+
+function initMap(){
+  var options = {
+    zoom:8,
+    center: {lat: 33.428, lng: -111.936}
+  }
+  var map = new google.maps.Map(document.getElementById("map"). options);
+}
